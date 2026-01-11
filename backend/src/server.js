b536c819779bfc5ab4596e7ffa406348d0810cde
Gabriela Froń <gabriela.fron04@gmail.com>
2026-01-11 21:35:49 +0100

    add app get

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,11 +8,21 @@ dotenv.config()
 
 const app = express()
 
+
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
 
 app.use("/api", apiRouter)
+
+app.get("/", (req, res) => {
+  res.status(200).send("OK")
+})
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ ok: true })
+})
+
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, "0.0.0.0", () => {
